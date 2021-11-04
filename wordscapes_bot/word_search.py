4d399cd5619b1eb34ecebbf28d981ece8d8be6aa
AFDtea <77896483+AFDtea@users.noreply.github.com>
2021-11-03 21:41:10 -0400

    Update word_search.py

--- wordscapes_bot/word_search.py
+++ wordscapes_bot/word_search.py
@@ -11,13 +11,28 @@ for i in words:
 # Closing file
 words.close()
 
-dictionary = ["Hello", "Testing", "Testing", "hi", "hey", "hip"]
-
 possibleWords = []
-def dictTest(letters):
-  for i in range(len(dictionary)):
-    if(len(letters) >= len(dictionary[i]) && len(dictionary[i]) > 2):
-      possibleWords.append(dictionary[i])
+actualWords = []
 
-dictTest(["h", "e", "y"])
+
+def dict_test(letters):
+    for i in range(len(dictionary)):
+        if len(letters) >= len(dictionary[i]) > 2:
+            possibleWords.append(dictionary[i])
+
+    for i in range(len(possibleWords)):
+        count = 0
+        temp_letters = []
+        for j in letters:
+            temp_letters.append(j)
+        for j in range(len(possibleWords[i])):
+            if possibleWords[i][j].lower() in temp_letters:
+                count += 1
+                temp_letters.remove(possibleWords[i][j].lower())
+        if count == len(possibleWords[i]):
+            actualWords.append(possibleWords[i])
+
+
+dict_test(["h", "e", "l", 'l', 'l', 'o'])
 print(possibleWords)
+print(actualWords)
