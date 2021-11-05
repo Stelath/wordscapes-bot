0c6eddef68e2b57fe3c141d098adab144e570af9
Alex <alex.k.korteacounts@gmail.com>
2021-11-05 15:28:24 -0400

    Updated Input Function

--- wordscapes_bot/wordscapes_input/input.py
+++ wordscapes_bot/wordscapes_input/input.py
@@ -1,11 +1,10 @@
 from pynput.mouse import Button, Controller
+import time
 
 
 def input_word(word, input_characters):
     mouse = Controller()
     word_character_list = list(word)
-
-    mouse.press(Button.left)
 
     for character in word_character_list:
         character_position = input_characters[character]
@@ -13,5 +12,9 @@ def input_word(word, input_characters):
         y = character_position['y'] + (character_position['h'] / 2)
 
         mouse.position = (x, y)
+        mouse.press(Button.left)
+        print(f'Moved mouse to {mouse.position}')
+
+        time.sleep(1)
 
     mouse.release(Button.left)
--- wordscapes_bot/wordscapes_ocr/ocr.py
+++ wordscapes_bot/wordscapes_ocr/ocr.py
@@ -21,7 +21,7 @@ def ocr_characters(image):
 
             conf = '--psm 10 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
             character_string = pytesseract.image_to_string(crop_img, lang='eng', config=conf)[0:1].lower()
-            
+
             character_position = {'x': x, 'y': y, 'w': w, 'h': h}
             characters[character_string] = character_position
 
